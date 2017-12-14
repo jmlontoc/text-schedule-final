fc6df6d824c9b5b974d377b22a0cc4c43c80bd4a
paolomanlapaz <32410303+paolomanlapaz@users.noreply.github.com>
2017-12-14 16:21:42 +0800

    Added navigation in tabs

--- app/src/main/java/mobidev/dlsu/edu/textschedulefinal/MainActivity.java
+++ app/src/main/java/mobidev/dlsu/edu/textschedulefinal/MainActivity.java
@@ -51,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
 
         // once the tabs are clicked
 
-
         // auto reply
         autoReplyTab.setOnClickListener(new View.OnClickListener() {
             @Override
